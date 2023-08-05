ba07e87b82c1e65e642b84825a61eda04506ec96
Vishnu <Vishnu@Vishnu>
2023-08-05 14:24:56 +0530

    Change Sample commit

---
+++ src/test/java/sample/ChangeSample.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class ChangeSample {
+	
+	@Test
+	public void changeSample() {
+		System.out.println("Hey this is my new commit");
+	}
+
+}
