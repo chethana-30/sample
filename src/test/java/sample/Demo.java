ba0bed6d2257518ab8e0cb7a5aca6012c4792e19
Vishnu <Vishnu@Vishnu>
2023-08-05 13:11:22 +0530

    Demo is comitted

--- src/test/java/sample/Demo.java
+++ src/test/java/sample/Demo.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class Demo {
 	@Test
 	public void sample() {
-		System.out.println("This is my first commit");
+		System.out.println("This is my first committ");
 	}
 
 }
